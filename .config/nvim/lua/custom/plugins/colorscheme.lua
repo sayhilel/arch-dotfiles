07f4c0440be3d1dbc4ad5e56944221bbd18081bf
Sahil Sinha <dev.sahilsinha@gmail.com>
2024-03-15 15:15:21 -0700

    Please Work

---
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require('onedark').setup {
+        -- Set a style preset. 'dark' is default.
+        style = 'warmer'
+      }
+      require('onedark').load()
+    end,
+  }
+

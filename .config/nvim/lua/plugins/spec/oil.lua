2fd27e777323fca4386d60d1b50d9982eaeef03e
Sahil <dev.sahilsinha@gmail.com>
2025-07-03 15:38:05 -0400

    rebases my config

--- .config/nvim/lua/plugins/spec/oil.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"stevearc/oil.nvim",
-	opts = {},
-	dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	config = function()
-		require("oil").setup()
-		vim.keymap.set('n','-', '<CMD>Oil<CR>', {desc = 'Open parent directory'})
-	end,
-}

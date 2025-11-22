42e6e624a0f0060bcf9e86096bb6f88736415292
Sahil <dev.sahilsinha@gmail.com>
2025-11-22 11:44:08 -0700

    Niri switch prep

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,11 +1,10 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
-
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
@@ -14,7 +13,7 @@ config.initial_rows = 28
 config.font_size = 14
 
 config.window_frame = {
-	font_size = 14
+	font_size = 14,
 }
 
 -- Finally, return the configuration to wezterm:
